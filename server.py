46e1363ad6f53547f2043000f650a5c403ad549a
Zach Goldberg <zgold@corvette.(none)>
2010-03-31 00:35:52 -0400

    Fix some bugs in showing time for server Implement sending of station list and changing of current station Station changing works but is a bit buggy

--- server.py
+++ server.py
@@ -39,7 +39,9 @@ def save_pandora_song_info(title, artist, album, love):
 def save_pandora_time_info(timeinfo):
     stime = re.match("^.+(\d+\:\d+)\/(\d+\:\d\d)$", timeinfo)
     CLIENT.total_time = time_to_int("00:" + stime.group(2))
-    CLIENT.elapsed_time = CLIENT.total_time - time_to_int("00:" + stime.group(1))
+    CLIENT.elapsed_time = (CLIENT.total_time - 
+                           time_to_int("00:" + stime.group(1)))
+
 
 def setup_pandora():
     c = pandora.Config()
@@ -49,7 +51,6 @@ def setup_pandora():
 
     CLIENT.song_callback = save_pandora_song_info
     CLIENT.second_callback = save_pandora_time_info
-
 
 rd = setup_server()
 print "UPnP MediaRenderer Service Exported"
@@ -76,7 +77,8 @@ def handle_position_request(service, action):
     item = w.add_item()
     item.set_title(getattr(CLIENT, "title", ""))
     item.set_artist(getattr(CLIENT, "artist", ""))
-    item.set_album(getattr(CLIENT, "album", ""))
+    item.set_album("%s (%s)" % (getattr(CLIENT, "album", ""),
+                                getattr(CLIENT, "station")))
 
     action.set_value("Track", "0")
     action.set_value("TrackMetaData", w.get_string())
@@ -116,16 +118,62 @@ def pandora_next(service, action):
   CLIENT.next()
   getattr(action, "return")()
 
+def set_pandora_uri(service, action, uri):
+    print "Playing %s" % uri
+    match = re.search("/station\/(.*)$", uri)
+    if not match:
+        action.return_error(0, "Invalid URI")
+        
+    station = match.groups()[0]
+    print station
+    CLIENT.setStation(station)
+
+    getattr(action, "return")()
+
+def list_stations(service, action):
+    w = GUPnPAV.GUPnPDIDLLiteWriter.new("English")
+
+    for station in CLIENT.stations:
+        item = w.add_item()
+        item.set_title(station)
+        uri = "http://%s:%s/station/%s" % (
+            CONTEXT.get_host_ip(),
+            CONTEXT.get_port(),
+            station
+            )
+
+        res = item.add_resource()
+        res.set_uri(uri)
+        
+    action.set_value("Result", w.get_string())
+    action.set_value("NumberReturned", len(CLIENT.stations))
+    action.set_value("TotalMatches", len(CLIENT.stations))
+    action.set_value("UpdateID", "0")
+
+    getattr(action, "return")()
+
+def handle_uri_change(service, action):
+    uri = action.get_value_type("CurrentURI", GObject.TYPE_STRING)
+    print "Change URI: %s" % uri
+    if not uri:
+        getattr(action, "return")()
+        return None
+
+    if CONTEXT.get_host_ip() in uri and str(CONTEXT.get_port()) in uri:
+        return set_pandora_uri(service, action, uri)
+    else:
+        action.return_error(0, "Invalid URI")
+
 service = rd.get_service("urn:schemas-upnp-org:service:AVTransport:1")
 service.connect("action-invoked::Play", pandora_play)
 service.connect("action-invoked::Pause", pandora_play)
 service.connect("action-invoked::Next", pandora_next)
-#service.connect("action-invoked::SetAVTransportURI", handle_uri_change)
+service.connect("action-invoked::SetAVTransportURI", handle_uri_change)
 service.connect("action-invoked::GetTransportInfo", handle_state_request)
 service.connect("action-invoked::GetPositionInfo",  handle_position_request)
 
-#directory = rd.get_service("urn:schemas-upnp-org:service:ContentDirectory:1")
-#directory.connect("action-invoked::Browse", browse_action)
+directory = rd.get_service("urn:schemas-upnp-org:service:ContentDirectory:1")
+directory.connect("action-invoked::Browse", list_stations)
 
 print "Awaiting commands..."
 try:
@@ -134,86 +182,5 @@ except KeyboardInterrupt:
     print "Done"
     sys.exit(0)
 
-
-
-
-def set_pandora_uri(service, action, uri):
-    print "Playing %s" % uri
-    match = re.search("/file\/(.*)$", uri)
-    if not match:
-        action.return_error(0, "Invalid URI")
-        
-    itemid = int(match.groups()[0])
-    
-    song = LIBRARY.get_by_id(itemid)
-
-    if not isinstance(song, MPDSong):
-        action.return_error()
-        return
-    
-    MPDCLIENT.connect()
-    songdata = MPDCLIENT.playlistfind('file', song.file)
-    
-    if songdata:        
-        # If the song is in the current playlist move to it and play it
-        MPDCLIENT.seek(songdata[0]['pos'], 0)
-    else:
-        # Else add it to the playlist then play it
-        MPDCLIENT.add(song.file)
-        songdata = MPDCLIENT.playlistfind('file', song.file)
-        if not songdata:
-            action.return_error()
-            return
-        MPDCLIENT.seek(songdata[0]['pos'], 0)
-
-    MPDCLIENT.disconnect()
-    getattr(action, "return")()
-
-
-def set_http_uri(service, action, uri):
-    """
-    This is a bit tricker.  We need to download the file from the local network
-    (hopefully its quick), add the file to MPD (the file has to be 100% downloaded first)
-    then add the file to the playlist and seek to it.
-
-    1) Download file
-    2) Add file to DB
-    3) Load file to local library
-    4) Generate an pandora uri and then call set_pandora_uri
-    """
-    path = uri.replace("http:/", "")
-    filename = os.path.basename(path)
-
-    if not "." in filename:
-        filename += ".mp3" # assume mp3 for now
-    
-    os.system("wget %s -O %s/%s" % (uri, MUSIC_PATH, filename))
-    
-    LIBRARY.connect()
-    MPDCLIENT.update(filename)
-    
-    songdata = MPDCLIENT.find('file', filename)
-    if not songdata:
-        action.return_error(0, "Couldn't add file to MPD database")
-        return
-    
-    song_id = LIBRARY.register_song(LIBRARY.song_from_dict(songdata[0]))
-
-    LIBRARY.disconnect()
-    set_mpd_uri(service, action, "http://%s:%s/file/%s" % (
-        CONTEXT.get_host_ip(),
-        CONTEXT.get_port(),
-        song_id)
-                )
-    
-def handle_uri_change(service, action):
-    uri = action.get_value_type("CurrentURI", GObject.TYPE_STRING)
-    if not uri:
-      return None
-
-    if CONTEXT.get_host_ip() in uri and str(CONTEXT.get_port()) in uri:
-        return set_pandora_uri(service, action, uri)
-    else:
-        return set_http_uri(service, action, uri)
 
 
